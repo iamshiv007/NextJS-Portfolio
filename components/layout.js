01c0be9c69aebfc302fdfe1a81d99c80a5f37a2e
iamshiv007 <iamshiv20032003@gmail.com>
2023-06-01 18:12:59 +0530

    css-rare-selector completed

--- components/layout.js
+++ components/layout.js
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="CSS, HTML, Javascript, React blogs"
         />
         <meta
           property="og:image"
