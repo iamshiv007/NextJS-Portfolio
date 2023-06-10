a301ed2eb63bc502261c466fa09c027d490b5ae5
iamshiv007 <iamshiv20032003@gmail.com>
2023-06-10 18:10:40 +0530

    tic-tac-toe completed

--- pages/posts/tic-tac-toe.jsx
+++ pages/posts/tic-tac-toe.jsx
@@ -6,6 +6,8 @@ import React, { Fragment, useEffect, useState } from "react";
 
 const TicTacToe = () => {
   const [game, setGame] = useState(0);
+  const [over, setOver] = useState(false);
+  const [winer, setWiner] = useState();
 
   useEffect(() => {
     const ones = document.getElementsByClassName("one");
@@ -16,88 +18,104 @@ const TicTacToe = () => {
     const sixs = document.getElementsByClassName("six");
     const sevens = document.getElementsByClassName("seven");
     const eights = document.getElementsByClassName("eight");
-    const nines = document.getElementsByClassName("nine");
 
     if (
       (ones[0].innerText === "0" &&
         ones[1].innerText === "0" &&
         ones[2].innerText === "0") ||
-      (ones[0].innerText === "1" &&
-        ones[1].innerText === "1" &&
-        ones[2].innerText === "1")
+      (ones[0].innerText === "❌" &&
+        ones[1].innerText === "❌" &&
+        ones[2].innerText === "❌")
     ) {
-      alert("one is  won");
+      setOver(true);
+      setWiner(ones[0].innerText);
     } else if (
       (twos[0].innerText === "0" &&
         twos[1].innerText === "0" &&
         twos[2].innerText === "0") ||
-      (twos[0].innerText === "1" &&
-        twos[1].innerText === "1" &&
-        twos[2].innerText === "1")
+      (twos[0].innerText === "❌" &&
+        twos[1].innerText === "❌" &&
+        twos[2].innerText === "❌")
     ) {
-      alert("two is  won");
+      setOver(true);
+      setWiner(twos[0].innerText);
     } else if (
       (threes[0].innerText === "0" &&
         threes[1].innerText === "0" &&
         threes[2].innerText === "0") ||
-      (threes[0].innerText === "1" &&
-        threes[1].innerText === "1" &&
-        threes[2].innerText === "1")
+      (threes[0].innerText === "❌" &&
+        threes[1].innerText === "❌" &&
+        threes[2].innerText === "❌")
     ) {
-      alert("three is  won");
+      setOver(true);
+      setWiner(threes[0].innerText);
     } else if (
       (fours[0].innerText === "0" &&
         fours[1].innerText === "0" &&
         fours[2].innerText === "0") ||
-      (fours[0].innerText === "1" &&
-        fours[1].innerText === "1" &&
-        fours[2].innerText === "1")
+      (fours[0].innerText === "❌" &&
+        fours[1].innerText === "❌" &&
+        fours[2].innerText === "❌")
     ) {
-      alert("four is  won");
+      setOver(true);
+      setWiner(fours[0].innerText);
     } else if (
       (fives[0].innerText === "0" &&
         fives[1].innerText === "0" &&
         fives[2].innerText === "0") ||
-      (fives[0].innerText === "1" &&
-        fives[1].innerText === "1" &&
-        fives[2].innerText === "1")
+      (fives[0].innerText === "❌" &&
+        fives[1].innerText === "❌" &&
+        fives[2].innerText === "❌")
     ) {
-      alert("five is  won");
+      setOver(true);
+      setWiner(fives[0].innerText);
     } else if (
       (sixs[0].innerText === "0" &&
         sixs[1].innerText === "0" &&
         sixs[2].innerText === "0") ||
-      (sixs[0].innerText === "1" &&
-        sixs[1].innerText === "1" &&
-        sixs[2].innerText === "1")
+      (sixs[0].innerText === "❌" &&
+        sixs[1].innerText === "❌" &&
+        sixs[2].innerText === "❌")
     ) {
-      alert("six is  won");
+      setOver(true);
+      setWiner(sixs[0].innerText);
     } else if (
       (sevens[0].innerText === "0" &&
         sevens[1].innerText === "0" &&
         sevens[2].innerText === "0") ||
-      (sevens[0].innerText === "1" &&
-        sevens[1].innerText === "1" &&
-        sevens[2].innerText === "1")
+      (sevens[0].innerText === "❌" &&
+        sevens[1].innerText === "❌" &&
+        sevens[2].innerText === "❌")
     ) {
-      alert("seven is  won");
+      setOver(true);
+      setWiner(sevens[0].innerText);
     } else if (
       (eights[0].innerText === "0" &&
         eights[1].innerText === "0" &&
         eights[2].innerText === "0") ||
-      (eights[0].innerText === "1" &&
-        eights[1].innerText === "1" &&
-        eights[2].innerText === "1")
+      (eights[0].innerText === "❌" &&
+        eights[1].innerText === "❌" &&
+        eights[2].innerText === "❌")
     ) {
-      alert("eight is  won");
+      setOver(true);
+      setWiner(eights[0].innerText);
     }
   }, [game]);
 
   const gameFun = (e) => {
-    if (!e.target.innerText) {
+    if (!e.target.innerText && !over) {
       e.target.innerText = game;
-      setGame(game ? 0 : 1);
+      setGame(game ? 0 : "❌");
     }
+  };
+
+  const resetGame = () => {
+    const allSquares = document.getElementsByClassName("square");
+    for (let i = 0; i < 9; i++) {
+      allSquares[i].innerText = null;
+    }
+
+    setOver(false);
   };
 
   return (
@@ -117,6 +135,21 @@ const TicTacToe = () => {
             <SecondRow gameFun={gameFun} />
             <ThirdRow gameFun={gameFun} />
           </div>
+          {over && (
+            <div className="flex items-center justify-center flex-col my-5">
+              <div className="text-2xl">
+                {" "}
+                <span className="text-3xl font-bold">{winer}</span> is won the
+                game
+              </div>
+              <button
+                onClick={resetGame}
+                className="rounded bg-red-500 text-xl py-1 px-4 my-2 text-white"
+              >
+                Restart
+              </button>
+            </div>
+          )}
         </div>
       </Theme>
     </Fragment>
@@ -129,9 +162,9 @@ const FirstRow = ({ gameFun }) => {
   return (
     <>
       <div>
-        <div onClick={gameFun} className="one four seven"></div>
-        <div onClick={gameFun} className="one five"></div>
-        <div onClick={gameFun} className="one six eight"></div>
+        <div onClick={gameFun} className="square one four seven"></div>
+        <div onClick={gameFun} className="square one five"></div>
+        <div onClick={gameFun} className="square one six eight"></div>
       </div>
     </>
   );
@@ -141,9 +174,9 @@ const SecondRow = ({ gameFun }) => {
   return (
     <>
       <div>
-        <div onClick={gameFun} className="two four"></div>
-        <div onClick={gameFun} className="two five eight seven"></div>
-        <div onClick={gameFun} className="two six"></div>
+        <div onClick={gameFun} className="square two four"></div>
+        <div onClick={gameFun} className="square two five eight seven"></div>
+        <div onClick={gameFun} className="square two six"></div>
       </div>
     </>
   );
@@ -153,9 +186,9 @@ const ThirdRow = ({ gameFun }) => {
   return (
     <>
       <div>
-        <div onClick={gameFun} className="three four eight"></div>
-        <div onClick={gameFun} className="three five"></div>
-        <div onClick={gameFun} className="three six seven"></div>
+        <div onClick={gameFun} className="square three four eight"></div>
+        <div onClick={gameFun} className="square three five"></div>
+        <div onClick={gameFun} className="square three six seven"></div>
       </div>
     </>
   );
