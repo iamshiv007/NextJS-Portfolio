588eddd8a9af72ac84e9ab441ffda08db957255e
iamshiv007 <iamshiv20032003@gmail.com>
2023-06-16 16:56:26 +0530

    masonary completed

--- components/ThemeBtn.jsx
+++ components/ThemeBtn.jsx
@@ -8,7 +8,7 @@ const ThemeBtn = () => {
   return (
     <div
       onClick={() => setDark(!dark)}
-      className="w-fit fixed bg-white right-8 bottom-6 rounded-full shadow-md shadow-gray-700 dark:shadow-white p-2 text-2xl dark:bg-black dark:text-white cursor-pointer z-10"
+      className="w-fit fixed bg-white right-6 md:right-8 bottom-6 rounded-full shadow-md shadow-gray-700 dark:shadow-white p-2 text-2xl dark:bg-black dark:text-white cursor-pointer z-10"
     >
       {dark ? <BsSun /> : <BsMoon />}
     </div>
