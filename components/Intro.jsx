a13cd850a7b62cf2bc28bc78bb705ca1902162bb
iamshiv007 <iamshiv20032003@gmail.com>
2023-06-24 23:27:03 +0530

    navbar updated

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -1,42 +1,43 @@
 import React, { Fragment } from "react";
 import { TypeAnimation } from "react-type-animation";
-import Navbar from "./Navbar";
+import { Navbar2 } from "./Navbar";
 
 const Intro = () => {
   return (
     <Fragment>
-        <div
-          className="myImage h-[100vh] bg-no-repeat bg-blend-multiply hidden md:block"
-          style={{
-            backgroundImage: `url(./images/shiv2.jpg), linear-gradient(to right, black, gray,  gray)`,
-            backgroundSize: "cover",
-          }}
-        >
-          <Navbar/>
-          <div className="p-4 md:ml-14 md:pt-14">
-            <img className="w-12 h-12 hiIcon" src="./images/hii.png" alt="" />{" "}
-            <h1 className="md:text-7xl text-4xl myName dark:text-blue-700 text-blue-700 mt-4 mb-8">
-              My Name is Shivraj.
-            </h1>
-            <MyProfession profession={profession} />
-          </div>
+      <div
+        className="myImage h-[100vh] bg-no-repeat bg-blend-multiply hidden md:block"
+        style={{
+          backgroundImage: `url(./images/shiv2.jpg), linear-gradient(to right, black, gray,  gray)`,
+          backgroundSize: "cover",
+        }}
+      >
+        <Navbar2 />
+        <div className="p-4 md:ml-14 md:pt-14">
+          <img className="w-12 h-12 hiIcon" src="./images/hii.png" alt="" />{" "}
+          <h1 className="md:text-7xl text-4xl myName dark:text-blue-700 text-blue-700 mt-4 mb-8">
+            My Name is Shivraj.
+          </h1>
+          <MyProfession profession={profession} />
         </div>
+      </div>
 
-        <div
-          className="myImage h-[92vh] bg-no-repeat bg-blend-multiply md:hidden block"
-          style={{
-            backgroundImage: `url(./images/shiv1.jpg), linear-gradient(gray, gray)`,
-            backgroundSize: "cover",
-          }}
-        >
-          <div className="p-4 md:ml-14 md:pt-14">
-            <img className="w-12 h-12 hiIcon" src="./images/hii.png" alt="" />{" "}
-            <h1 className="md:text-7xl text-3xl myName dark:text-blue-700 text-blue-700 mt-4 mb-8">
-              My Name is Shivraj.
-            </h1>
-            <MyProfession profession={profession} />
-          </div>
+      <div
+        className="myImage h-[92vh] bg-no-repeat bg-blend-multiply md:hidden block"
+        style={{
+          backgroundImage: `url(./images/shiv1.jpg), linear-gradient(gray, gray)`,
+          backgroundSize: "cover",
+        }}
+      >
+        <Navbar2 />
+        <div className="p-4 md:ml-14 md:pt-14">
+          <img className="w-12 h-12 hiIcon" src="./images/hii.png" alt="" />{" "}
+          <h1 className="md:text-7xl text-3xl myName dark:text-blue-700 text-blue-700 mt-4 mb-8">
+            My Name is Shivraj.
+          </h1>
+          <MyProfession profession={profession} />
         </div>
+      </div>
     </Fragment>
   );
 };
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import React, { Fragment } from "react";
 
-const Navbar = ({ children }) => {
+const Navbar = () => {
   return (
     <Fragment>
       <div className="navbar">
@@ -37,9 +37,50 @@ const Navbar = ({ children }) => {
           </div>
         </div>
       </div>
-      <div>{children}</div>
+    </Fragment>
+  );
+};
+
+const Navbar2 = () => {
+  return (
+    <Fragment>
+      <div className="navbar2">
+        <div>
+          <Link href="/">
+            <img className="h-9 mx-10" src="/images/Logo.ico" alt="" />
+          </Link>
+        </div>
+        <div className="flex items-center">
+          <div className="sm:mx-4 mx-1">
+            <Link
+              className="p-2 font-bold text-white hover:no-underline"
+              href="/blogs"
+            >
+              Blogs
+            </Link>
+          </div>
+          <div className="sm:mx-4 mx-1">
+            <Link
+              className="p-2 font-bold text-white hover:no-underline"
+              href="/about"
+            >
+              About
+            </Link>
+          </div>
+          <div className="sm:mx-4 mx-1">
+            <Link
+              className="p-2 font-bold text-white hover:no-underline"
+              href="/contacts"
+            >
+              Contacts
+            </Link>
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 };
 
 export default Navbar;
+
+export { Navbar2 };
