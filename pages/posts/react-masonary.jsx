92bc2f28457bea0f8d71a5861f404a0a981f45dd
iamshiv007 <iamshiv20032003@gmail.com>
2023-06-27 17:53:08 +0530

    react masonary now we have a download button

--- pages/posts/react-masonary.jsx
+++ pages/posts/react-masonary.jsx
@@ -19,10 +19,29 @@ const ReactMasonary = () => {
     axios
       .get(`https://api.pexels.com/v1/curated?page=${1}`, config)
       .then((res) => {
+        console.log(res.data.photos);
         setPhotos(res.data.photos);
       })
       .catch((err) => console.log(err));
   }, []);
+
+  const handleDownload = async (imageUrl, myurl) => {
+    try {
+      const response = await fetch(imageUrl);
+      const blob = await response.blob();
+      const url = URL.createObjectURL(blob);
+
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = `${myurl.split("/")[myurl.split("/").length - 2]}.jpeg`;
+      link.click();
+
+      // Clean up the temporary URL
+      URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error("Error downloading image:", error);
+    }
+  };
 
   return (
     <Fragment>
@@ -35,15 +54,25 @@ const ReactMasonary = () => {
         <ThemeBtn />
 
         <div className="dark:bg-black min-h-92">
+          <div className="dowBtn"></div>
           <ResponsiveMasonry
             columnsCountBreakPoints={{ 350: 2, 750: 3, 900: 4 }}
           >
             <Masonry>
-              {photos.map((photo, key) => (
-                <div style={{ margin: "10px" }} key={key}>
-                  <img src={photo.src.original} alt={photo.photographer} />
-                </div>
-              ))}
+              {photos.map((photo, key) => {
+                return (
+                  <div style={{ margin: "10px" }} key={key}>
+                    <button
+                      onClick={() =>
+                        handleDownload(photo.src.original, photo.url)
+                      }
+                    >
+                      Download
+                    </button>
+                    <img src={photo.src.original} alt={photo.photographer} />
+                  </div>
+                );
+              })}
             </Masonry>
           </ResponsiveMasonry>
         </div>
