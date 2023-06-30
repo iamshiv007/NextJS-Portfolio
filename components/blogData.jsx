111a7d164cea28e61b4c4f8b0d8a3c9a84f48eea
iamshiv007 <iamshiv20032003@gmail.com>
2023-06-30 19:09:27 +0530

    arrange files

--- components/blogData.jsx
+++ components/blogData.jsx
@@ -10,56 +10,56 @@ export const blogs = [
     name: "CSS transform property",
     imageUrl:
       "https://tenten.vn/tin-tuc/wp-content/uploads/2022/05/css_transform-4.png",
-    linkName: "css-transform",
+    linkName: "css/css-transform",
     date: "May 29, 2023",
   },
   {
     name: "CSS background-blend-mode",
     imageUrl:
       "https://d33wubrfki0l68.cloudfront.net/a9e6f9b548512fc1e81455a6ca2d59e273d41bda/668f7/assets/images/blending-examples.png",
-    linkName: "blend-mode",
+    linkName: "css/blend-mode",
     date: "May 30, 2023",
   },
   {
     name: "CSS Rare Selectors",
     imageUrl:
       "https://internetingishard.netlify.app/css-selectors-1f0064.464e0c0e.png",
-    linkName: "css-rare-selectors",
+    linkName: "css/css-rare-selectors",
     date: "May 31, 2023",
   },
   {
     name: "Hoisting In Javasript",
     imageUrl:
       "https://cdn.hashnode.com/res/hashnode/image/upload/v1612248117684/PTiy4Ga2n.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webpy",
-    linkName: "Hoisting",
+    linkName: "javascript/Hoisting",
     date: "June 1, 2023",
   },
   {
     name: "React Responsive carousel",
     imageUrl:
       "https://reactscript.com/wp-content/uploads/2016/07/React.js-Responsive-Carousel.png",
-    linkName: "react-res-carousel",
+    linkName: "react/react-res-carousel",
     date: "June 2, 2023",
   },
   {
     name: "CSS Animation",
     imageUrl:
       "https://res.cloudinary.com/practicaldev/image/fetch/s--rNSGibyc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/54ydb37tzyny06ac8xdf.jpg",
-    linkName: "css-animation",
+    linkName: "css/css-animation",
     date: "June 5, 2023",
   },
   {
     name: "object-fit",
     imageUrl:
       "https://webdesign-trends.net/wp/wp-content/uploads/2020/05/object-fit-tate02-1024x697.png",
-    linkName: "object-fit",
+    linkName: "css/object-fit",
     date: "June 6, 2023",
   },
   {
     name: "linear-gradient",
     imageUrl:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxVjbf9Rf5VEHP_D6IiUYOksUi2OgEzhg2ny5DkeV5kA&usqp=CAU&ec=48600112",
-    linkName: "linear-gradient",
+    linkName: "css/linear-gradient",
     date: "June 7, 2023",
   },
   {
@@ -73,28 +73,28 @@ export const blogs = [
     name: "Tic-Tac-Toe",
     imageUrl:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJ76F-qBseNwI8hvVrpG3bthHX-JgZOT1EVqltRRYaSw&usqp=CAU&ec=48600112",
-    linkName: "tic-tac-toe",
+    linkName: "apps/tic-tac-toe",
     date: "June 9, 2023",
   },
   {
     name: "Drag And Drop",
     imageUrl:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvf3LxW2yGYlBT15yJsQmnXbGTSl-Q2_nKkZLJrvU2Hg&usqp=CAU&ec=48600112",
-    linkName: "drag-and-drop",
+    linkName: "apps/drag-and-drop",
     date: "June 15, 2023",
   },
   {
     name: "React Masonary",
     imageUrl:
       "https://reactjsexample.com/content/images/2018/11/React-Photo-Gallery.jpg",
-    linkName: "react-masonary",
+    linkName: "react/react-masonary",
     date: "June 16, 2023",
   },
   {
     name: "Css Grid",
     imageUrl:
       "https://www.freecodecamp.org/news/content/images/size/w2000/2022/05/CSS-GRID-3.png",
-    linkName: "css-grid",
+    linkName: "css/css-grid",
     date: "June 16, 2023",
   },
   {
@@ -115,63 +115,63 @@ export const blogs = [
     name: "Quiz App",
     imageUrl:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4WCmXtQ2AW5OsFElhZ0PCfpqR6dYPgqyV9Ossb3Smyg&usqp=CAU&ec=48600112",
-    linkName: "quiz-app",
+    linkName: "apps/quiz-app",
     date: "June 20, 2023",
   },
   {
     name: "Hindi Translator",
     imageUrl:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGg5kZa7RkIxqvqXflLuI0otBRpypPNBts1Ku29vxcqw&usqp=CAU&ec=48600112",
-    linkName: "hindi-translator",
+    linkName: "apps/hindi-translator",
     date: "June 21, 2023",
   },
   {
     name: "Apna Chat GPT",
     imageUrl:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlmA2CxPc1F6Fuim6MSL17w4QTctxSr6kI5QvxLvfLqA&usqp=CAU&ec=48600112",
-    linkName: "apna-chat-gpt",
+    linkName: "apps/apna-chat-gpt",
     date: "June 22, 2023",
   },
   {
     name: "News App",
     imageUrl:
       "https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg",
-    linkName: "news-app",
+    linkName: "apps/news-app",
     date: "June 23, 2023",
   },
   {
     name: "Music Chalao",
     imageUrl:
       "https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg",
-    linkName: "music-chalao",
+    linkName: "apps/music-chalao",
     date: "June 25, 2023",
   },
   {
     name: "Closure in Javascript",
     imageUrl:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxuSU_XV8gfjdse5Pl4LyDnVpmLvNEbjiii3yhgmLj_A&usqp=CAU&ec=48600112",
-    linkName: "closure",
+    linkName: "javascript/closure",
     date: "June 26, 2023",
   },
   {
     name: "Debouncing in Javascript",
     imageUrl:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFNt-rL1StGYk10YCMxez5SnHolxHC82nsVrNyBDjcJw&usqp=CAU&ec=48600112",
-    linkName: "debouncing",
+    linkName: "javascript/debouncing",
     date: "June 27, 2023",
   },
   {
     name: "Throttling in Javascript",
     imageUrl:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDXKGVPWZR3E56lLvZde0pb_uGNkq6A_UFQw&usqp=CAU",
-    linkName: "throttling",
+    linkName: "javascript/throttling",
     date: "June 28, 2023",
   },
   {
     name: "Rest and  Spread Operator",
     imageUrl:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1i7928wer2C-LN0mx0bdsp88row0sCsjREw&usqp=CAU",
-    linkName: "spreadOperator",
+    linkName: "javascript/spreadOperator",
     date: "June 29, 2023",
   },
 ];
