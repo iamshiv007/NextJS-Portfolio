f985a87201a43cbd4fb87e760fbcf5b63221b7ca
iamshiv007 <iamshiv20032003@gmail.com>
2023-07-14 10:56:39 +0530

    MobileScreenJourney created

--- components/Journey.jsx
+++ components/Journey.jsx
@@ -4,17 +4,22 @@ import Iit from "./Journey/Iit";
 import College from "./Journey/College";
 import YouTube from "./Journey/YouTube";
 import { GiRailRoad } from "react-icons/gi";
+import { JourneyData } from "./Journey/JourneyData";
 
 const Journey = () => {
   return (
     <Fragment>
       <section id="myJourney">
-        <div className="min-h-[110vh] pt-6 dark:bg-[#000000d0] overflow-x-hidden">
-          <h3 className="text-center md:mb-12 mb-3 flex items-center justify-center text-3xl md:text-5xl font-medium p-4 dark:text-[#07d0e5] text-[#c72c6c]">
-           <span className="mr-4"> <GiRailRoad /></span> My Journey
+        <div className="md:min-h-[110vh] h-auto pt-6 dark:bg-[#000000d0] overflow-x-hidden">
+          <h3 className="text-center md:mb-12 flex items-center justify-center text-3xl md:text-5xl font-medium p-4 dark:text-[#07d0e5] text-[#c72c6c]">
+            <span className="mr-4">
+              {" "}
+              <GiRailRoad />
+            </span>{" "}
+            My Journey
           </h3>
 
-          <div className="md:flex block justify-center pb-10">
+          <div className="md:flex hidden justify-center pb-10">
             <div className="py-5">
               <School />
 
@@ -27,7 +32,7 @@ const Journey = () => {
               </div>
             </div>
 
-            <div className="border-2 md:block hidden dark:border-white border-black border-solid min-h-[90vh]" />
+            <div class5ame="border md:block hidden dark:border-white border-black border-solid min-h-[90vh]" />
 
             <div className="py-5">
               <div className="md:block hidden">
@@ -41,6 +46,8 @@ const Journey = () => {
               <YouTube />
             </div>
           </div>
+
+          <MobileScreenJourney />
         </div>
       </section>
     </Fragment>
@@ -48,3 +55,62 @@ const Journey = () => {
 };
 
 export default Journey;
+
+const MobileScreenJourney = () => {
+  const visibleFun = (e) => {
+    const smallBorder =
+      e.target.parentElement.parentElement.nextElementSibling.classList;
+
+    const detailBox =
+      e.target.parentElement.parentElement.nextElementSibling.nextElementSibling
+        .classList;
+
+    if (smallBorder.contains("h-0")) {
+      smallBorder.add("h-[20px]");
+      smallBorder.remove("h-0");
+    } else {
+      smallBorder.add("h-0");
+      smallBorder.remove("h-[20px]");
+    }
+
+    if (detailBox.contains("h-0")) {
+      detailBox.add("h-auto");
+      detailBox.remove("h-0");
+    } else {
+      detailBox.add("h-0");
+      detailBox.remove("h-auto");
+    }
+  };
+  return (
+    <>
+      <div className="mx-[5%] pb-10 md:hidden">
+        {JourneyData.map((journey, key) => (
+          <React.Fragment key={key}>
+            <div className="h-[50px] border border-white m-auto w-fit"></div>
+
+            <div className="border border-white border-solid rounded">
+              <div className="flex gap-5 px-5 py-2">
+                <p onClick={visibleFun}>+</p>
+                <p>{journey.heading}</p>
+                <p className="ml-auto">{journey.time}</p>
+              </div>
+            </div>
+
+            <div className="border border-white m-auto w-fit h-0"></div>
+
+            <div className="border-white h-0 overflow-hidden">
+              <img
+                className="w-full"
+                src={journey.image}
+                alt={journey.heading}
+              />
+              <div className="py-2">
+                <ul className="list-inside list-disc">{journey.summary}</ul>
+              </div>
+            </div>
+          </React.Fragment>
+        ))}
+      </div>
+    </>
+  );
+};
---
+++ components/Journey/JourneyData.jsx
@@ -0,0 +1,34 @@
+export const JourneyData = [
+  {
+    heading: "School",
+    time: "2015-2019",
+    image:
+      "https://scontent.fjdh1-1.fna.fbcdn.net/v/t1.18169-9/11180626_932953900101923_2418688841280992475_n.jpg?_nc_cat=100&cb=99be929b-3346023f&ccb=1-7&_nc_sid=174925&_nc_ohc=p5k0xy1wrJwAX998ZzW&_nc_ht=scontent.fjdh1-1.fna&oh=00_AfAJcbQ7R6X1KMe0cbfgf95MvJN1DnU2fThfh9rSI2wolg&oe=64D624A8",
+    summary:
+      "Topper at School time A Mathematics Guy shy and Introverted personality Always Boyz friend Zone",
+  },
+  {
+    heading: "IIT Preparation",
+    time: "2019-2021",
+    image:
+      "https://www.besteducationsikar.com/wp-content/uploads/2021/11/2019-06-15-800x600.jpg",
+    summary:
+      "Because I was a topper during my school days, some of my friends suggested that I pursue IIT. Additionally, my father's friend recommended IIT to my father for me. As a result, I went to Kota for IIT preparation. However, after my first attempt, I decided to quit IIT.",
+  },
+  {
+    heading: "BSC College",
+    time: "2021-2022",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3xSTnbK57kUx2XgDd_zzXJp5uXPSNUmeZ9g&usqp=CAUF",
+    summary:
+      "After quite IIT again, without reason i jumped to the BSC, actually i am finding to my self what can i do in my life in which thing i should made my carrier. In end of First year i decided to go in to tech field so quite bsc after completing first year exams.",
+  },
+  {
+    heading: "Coding Journey",
+    time: "2022-Ongoing",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbb0NTA9qMmGFF4NPZgMqebIfkPpOpK7TWlQ&usqp=CAU",
+    summary:
+      " After dropped out to college i decided to learn coding from YouTube and i started with Java basics and then i learn html, css, javascript and moved into web development in react js and in to MERN stack.",
+  },
+];
