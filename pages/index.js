4ea5bb42fdd0cc7ff1aec021c0bcd395019c9266
iamshiv007 <iamshiv20032003@gmail.com>
2023-08-06 14:46:13 +0530

    Meta tags updated - index

--- pages/index.js
+++ pages/index.js
@@ -21,41 +21,61 @@ export default function Home() {
     <Fragment>
       <Head>
         <title>Shiv's Portfolio</title>
-        <meta name="description" content="blogs about React, JavaScript, CSS, Next.js, React best Libraries, Github, Git, Web App, Games, Rapid Apis, Free Apis, learn css, css Example, play with css, small projects, and advanced concepts." />
-        <meta name="keywords" content="React Projects, Learn JavaScript, Learn CSS, Learn Next.js, small projects, advanced concepts, blog, Shivraj Gurjar Blog, Shivraj, React best Libraries, Github, Git, how To make simple Web App, how to make simple Games, best free Rapid Apis, How to use Free Rapid Apis, learn css, css Example, play with css" />
-        <meta property="og:title" content="My Blog" />
-        <meta property="og:description" content="blogs about React, JavaScript, CSS, Next.js, React best Libraries, Github, Git, Web App, Games, Rapid Apis, Free Apis, learn css, css Example, play with css, small projects, and advanced concepts." />
-        <meta property="og:image" content="./images/Logo.ico" />
-        <meta name="author" content="Shivraj" />
-      </Head>
-      <div className={dark ? "dark" : ""}>
-        <div className="dark:bg-gray-700 ">
-          {/* Desktop Navbar */}
-          <Navbar />
-          {/* Mobile Navbar */}
-          <MobileNavbar />
-          {/* Welcome Page */}
-          <Intro />
-          {/* SocialMedia */}
-          <SocialMedia />
-          {/* Skills  */}
-          <Skills />
-          {/* Projects */}
-          <Projects />
-          {/* LatestBlog */}
-          <LatestBlogs />
-          {/* Education Experience Journey */}
-          <Journey />
-          {/* Apps */}
-          <Apps />
-          {/* Send Mail */}
-          <SendMail />
-          {/* Footer */}
-          <Footer />
+        {/* <!-- Primary Meta Tags --> */}
+        <meta name="title" content="Shiv's Portfolio"/>
+          <meta name="description" content="Explore the best Next.js and React.js portfolio by Shivraj Gurjar. Featuring simple, dark and light theme designs and mobile responsiveness."/>
 
-          <ChatSystem />
-        </div>
-      </div>
-    </Fragment>
-  );
+            {/* <!-- Open Graph / Facebook --> */}
+            <meta property="og:type" content="website"/>
+              <meta property="og:url" content="https://shiv-s-portfolio.vercel.app"/>
+                <meta property="og:title" content="Shiv's Portfolio"/>
+                  <meta property="og:description" content="Explore the best Next.js and React.js portfolio by Shivraj Gurjar. Featuring simple, dark and light theme designs and mobile responsiveness."/>
+                    <meta property="og:image" content="https://shiv-s-portfolio.vercel.app/images/welcome-screen.png"/>
+
+                      {/* <!-- Twitter --> */}
+                      <meta property="twitter:card" content="https://shiv-s-portfolio.vercel.app/images/welcome-screen.png"/>
+                        <meta property="twitter:url" content="https://shiv-s-portfolio.vercel.app"/>
+                          <meta property="twitter:title" content="Shivraj Gurjar - Portfolio"/>
+                            <meta property="twitter:description" content="Explore the best Next.js and React.js portfolio by Shivraj Gurjar. Featuring simple, dark and light theme designs and mobile responsiveness."/>
+                              <meta property="twitter:image" content="https://shiv-s-portfolio.vercel.app/images/welcome-screen.png"/>
+
+                                {/* <!-- Other Meta Tags --> */}
+                                <meta name="keywords" content="portfolio, Next.js, React.js, web development, dark theme, light theme, best portfolio github, best next js portfolio, how to make a portfolio, best react js portfolio github, best react js portfolio, best next js portfolio, react js developer in Kota, react js developer in Jaipur, react js developer in India, react js developer in Rajasthan"/>
+                                  <meta name="author" content="Shivraj Gurjar"/>
+                                    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+                                      {/* <!-- Favicon --> */}
+                                      <link rel="icon" href="https://shiv-s-portfolio.vercel.app/favicon.ico" type="image/x-icon"/>
+
+                                      </Head>
+                                      <div className={dark ? "dark" : ""}>
+                                        <div className="dark:bg-gray-700 ">
+                                          {/* Desktop Navbar */}
+                                          <Navbar />
+                                          {/* Mobile Navbar */}
+                                          <MobileNavbar />
+                                          {/* Welcome Page */}
+                                          <Intro />
+                                          {/* SocialMedia */}
+                                          <SocialMedia />
+                                          {/* Skills  */}
+                                          <Skills />
+                                          {/* Projects */}
+                                          <Projects />
+                                          {/* LatestBlog */}
+                                          <LatestBlogs />
+                                          {/* Education Experience Journey */}
+                                          <Journey />
+                                          {/* Apps */}
+                                          <Apps />
+                                          {/* Send Mail */}
+                                          <SendMail />
+                                          {/* Footer */}
+                                          <Footer />
+
+                                          <ChatSystem />
+                                        </div>
+                                      </div>
+                                    </Fragment>
+                                    );
 }
